0f792759a3d3fb1106a27f4e95b5d465fd5566d7
AlbertLara <thepivot88@gmail.com>
2021-02-20 18:45:23 +0100

    First commit

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__,template_folder='web/templates',
             static_folder='web/static')
-
+print("Hi")
 
 if __name__=='__main__':
     app.run(host="localhost",port=8080,
