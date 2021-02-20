0d2e840f16cc4f755ff723566e84793c9f8e5365
AlbertLara <thepivot88@gmail.com>
2021-02-20 18:42:58 +0100

    First commit

---
+++ schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS public."Datos" (
+    id SERIAL PRIMARY KEY,
+    concepto VARCHAR(255) NOT NULL,
+    dia DATETIME NOT NULL,
+    dinero FLOAT NOT NULL,
+    idtransaction VARCHAR(3) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS public."Cuentas" (
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    inicial FLOAT NOT NULL
+)
+
+CREATE TABLE IF NOT EXISTS public."Gastos" (
+    id SERIAL PRIMARY KEY,
+    concepto VARCHAR(255) NOT NULL,
+    mes DATETIME NOT NULL
+)
