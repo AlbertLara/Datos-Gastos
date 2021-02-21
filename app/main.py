a798f541e469bbd7667f2f17dff21ec7cb178236
AlbertLara <thepivot88@gmail.com>
2021-02-21 12:04:38 +0100

    Add heroku.yml

--- app/main.py
+++ app/main.py
@@ -11,4 +11,4 @@ def index():
 
 if __name__=='__main__':
     port = int(os.getenv('PORT',5000))
-    app.run(host="0.0.0.0", debug=True, port=5000)
+    app.run(host="0.0.0.0", debug=True, port=port)
