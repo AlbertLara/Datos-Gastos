7609fc573bf155b56db71bda97cb75e2f4fef268
AlbertLara <thepivot88@gmail.com>
2021-02-21 14:58:04 +0100

    .

--- app/project/__init__.py
+++ app/project/__init__.py
@@ -4,9 +4,10 @@ def create_app():
     app = Flask(__name__,template_folder='web/templates',
                 static_folder='web/static')
     register_blueprint(app)
+    print(1)
     return app
 
 
 def register_blueprint(app:Flask):
-    from .endpoints.home import blueprint as home_blueprint
+    from project.endpoints.home import blueprint as home_blueprint
     app.register_blueprint(home_blueprint)
