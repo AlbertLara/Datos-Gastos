269ffa376638f0b219e030f75770f69ff5fdb3fd
AlbertLara <thepivot88@gmail.com>
2021-02-21 15:27:23 +0100

    .

--- app/project/endpoints/home/views.py
+++ app/project/endpoints/home/views.py
@@ -1,8 +1,10 @@
 from . import blueprint
 from flask import redirect, render_template, url_for, request
+import os
 
 @blueprint.route('/')
 def index():
     print("Hello 1")
+    print(os.environ)
     return render_template('home/index.html',
                            path='home',title='Welcome')
