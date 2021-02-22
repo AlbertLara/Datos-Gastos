41c0a7960801ced08668d66c2e238199af39b92c
AlbertLara <thepivot88@gmail.com>
2021-02-22 08:11:16 +0100

    Init database

--- app/project/__init__.py
+++ app/project/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from .models import db
 
 def create_app():
     app = Flask(__name__,template_folder='web/templates',
                 static_folder='web/static')
     app.config.from_pyfile("config.py")
-    
+    db.init_app(app)
+    with app.app_context():
+      db.create_all()
     register_blueprint(app)
     return app
 
---
+++ app/project/models.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Datos(db.Model):
+  __tablename__="Datos"
+  id = db.Column(db.Integer, primary_key=True,nullable=False)
+  concepto = db.Column(db.String(255),nullable=False)
+  dia = db.Column(db.DateTime,nullable=False)
+  dinero = db.Column(db.Float,nullable=False)
+  idtransaction = db.Column(db.String(3),nullable=False)
+  
+class Cuentas(db.Model):
+  __tablename__="Cuentas"
+  id = db.Column(db.Integer, primary_key=True,nullable=False)
+  nombre = db.Column(db.String(255),nullable=False)
+  inicial = db.Column(db.Float,nullable=False)
