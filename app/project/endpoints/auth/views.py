90a5ac6f3de8cf9ce3c41db2d97575b55b0b7ba0
AlbertLara <thepivot88@gmail.com>
2021-02-24 20:03:22 +0100

    .

--- app/project/endpoints/auth/views.py
+++ app/project/endpoints/auth/views.py
@@ -8,7 +8,8 @@ import os
 def login():
   form = LoginForm()
   if form.validate_on_submit():
-    
+    user = User.query.filter_by(email=form.email.data).first()
+    print(user)
     return redirect(url_for("auth.login"))
   template = render_template('auth/login.html', form=form,title='Login')
   return template
