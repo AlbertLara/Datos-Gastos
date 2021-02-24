d405b16054acf2f913bac5ba4770cb5e7ae8dd7a
AlbertLara <thepivot88@gmail.com>
2021-02-24 07:39:01 +0100

    .

--- app/project/__init__.py
+++ app/project/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .models import db
+from .models import db, User
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 import os
@@ -17,7 +17,9 @@ def create_app():
     with app.app_context():
       db.create_all()
     
-    
+    @login_manager.user_loader
+    def load_user(user_id):
+      return User.query.get(int(user_id))
     
     register_blueprint(app)
     return app
