8588318f87e9ac482bb53b6883ce25eefbf1245d
AlbertLara <thepivot88@gmail.com>
2021-02-26 14:52:14 +0100

    .

--- app/project/gspread_conn.py
+++ app/project/gspread_conn.py
@@ -4,3 +4,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 credential = ServiceAccountCredentials.from_json_keyfile_name("credentials.json",["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"])
 client = gspread.authorize(credential)
 spr = client.open("Gastos")
+
+sheets = spr.worksheets()
+
+print(sheets)
